139f14f0edcbbaccd3cbe5106265c3dd39ce034d
Keltoummalouki <keltoummalouki911@gmail.com>
2024-09-26 14:09:30 +0100

    Add files via upload

---
+++ C-language-exercises/Conditions/challenge7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+    //character : ch
+    int ch;
+    printf("Entrer la lettre : ");
+    scanf("%d", ch);
+
+    if 65<ch<90
+    printf("la lettre est majuscules");
+    else if 7<ch<122
+    printf("la lettre est ")
+
+
+
+    return 0;
+}
